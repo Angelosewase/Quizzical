2716f372805d3c2a88d5a77ca1a2947dfdfa03bd
Sewase Angel <sewasejo8@gmail.com>
2024-02-14 11:59:27 +0200

    improve  the design

--- src/pages/introPage.js
+++ src/pages/introPage.js
@@ -19,7 +19,16 @@ function Home(){
     <svg width="200" height="200">
       <polygon points="100,0 200,0 200,100 100,200 0,100" fill="#DEEBF8" />
     </svg>
+
+    <svg width="200" height="200" className="svg3">
+      <polygon points="100,0 300,0 400,100 100,200 0,100" fill="#DEEBF8" />
+    </svg>
+
+    <svg width="200" height="200" className="svg4">
+      <polygon points="200,0 300,0 200,100 100,300 0,100" fill="#FFFAD1" />
+    </svg>
     </>
+
  
    )
 }
